d64b31cf88be30ee44fde0ecec4858e035b7d907
ErickSkrauch <erickskrauch@yandex.ru>
2024-04-15 20:20:39 +0200

    Упрощена поддержка составных символов с диакритами

--- src/unicode.ts
+++ src/unicode.ts
@@ -10,13 +10,13 @@ const normalizationDictionary: Record<string, string> = {
     // Latin
     'D': '🄓Ꭰ🄳𝔡𝖉𝔻𝗗𝘋𝙳𝐷𝓓𝐃𝑫𝕯𝖣𝔇𝘿ꭰⅅ𝒟ꓓ🅳🅓ⒹＤƉᗪƊÐԺᴅᴰↁḊĐÞⅮᗞᑯĎḌḐḒḎᗫᗬᗟᗠᶛᴆ🇩Δ',
     'd': 'Ꮷ𝔡𝖉ᑯꓒ𝓭ᵭ₫ԃⓓｄḋďḍḑḓḏđƌɖɗᵈ⒟ⅾᶁᑺᑻᑼᑽᒄᑰᑱᶑ𝕕𝖽𝑑𝘥𝒅𝙙𝐝𝗱𝚍ⅆ𝒹ʠժδ',
-    'A': '𝑨𝔄ᗄ𝖠𝗔ꓯ𝞐🄐🄰Ꭿ𐊠𝕬𝜜𝐴ꓮᎪ𝚨ꭺ𝝖🅐Å∀Ɐ🇦₳🅰𝒜𝘈𝐀𝔸ǺᗅⒶＡΑᾋᗩĂÃÅǍȀȂĀȺĄʌΛλƛᴀᴬልÄₐᕱǞΆẠẢẦẨẬẮẰẲẴẶᾸᾹᾺΆᾼᾈᾉᾊᾌᾍᾎᾏἈἉἊἋἌἍἎἏḀȦǠÀÁÂẤẪ𝛢𝓐𝙰𝘼ᗩÆǢ𐌀A̱A̧Ą̇A҃',
-    'a': '∂⍺@ⓐձǟªᵃᶏ⒜ɒａαȃȁคǎმäɑāɐąᾄẚạảǡầẵḁȧãåάὰάăẩằẳặᾀᾁᾂᾃᾅᾆᾰᾱᾲᾳᾴᶐᾶᾷἀἁἂἃἄἅἆἇᾇậắàáâấẫǻⱥ𝐚𝑎𝒂𝒶𝓪𝔞𝕒𝖆𝖺𝗮𝘢𝙖𝚊𝛂𝛼𝜶𝝰𝞪⍶æǣa̱a̧ą̇a҃',
+    'A': '𝑨𝔄ᗄ𝖠𝗔ꓯ𝞐🄐🄰Ꭿ𐊠𝕬𝜜𝐴ꓮᎪ𝚨ꭺ𝝖🅐Å∀Ɐ🇦₳🅰𝒜𝘈𝐀𝔸ǺᗅⒶＡΑᾋᗩĂÃÅǍȀȂĀȺĄʌΛλƛᴀᴬልÄₐᕱǞΆẠẢẦẨẬẮẰẲẴẶᾸᾹᾺΆᾼᾈᾉᾊᾌᾍᾎᾏἈἉἊἋἌἍἎἏḀȦǠÀÁÂẤẪ𝛢𝓐𝙰𝘼ᗩÆǢ𐌀',
+    'a': '∂⍺@ⓐձǟªᵃᶏ⒜ɒａαȃȁคǎმäɑāɐąᾄẚạảǡầẵḁȧãåάὰάăẩằẳặᾀᾁᾂᾃᾅᾆᾰᾱᾲᾳᾴᶐᾶᾷἀἁἂἃἄἅἆἇᾇậắàáâấẫǻⱥ𝐚𝑎𝒂𝒶𝓪𝔞𝕒𝖆𝖺𝗮𝘢𝙖𝚊𝛂𝛼𝜶𝝰𝞪⍶æǣ',
     // Cyrillic
-    'Д': 'Д̌Д́Д̈Д̣Д̆ԀД̓Д̀Д̨ЂԂꚀꙢД҄',
-    'д': 'д̌д́д̈д̣д̆ԁд̓д̀д̨ђԃꚁꙣд҄',
-    'А': 'А̀А̂А̄ӒӐА̊А̃Ӓ̄ӔА̨ѦꙘА҃А̇',
-    'а': 'а́а̂а̄ӓӑа̊а̃ӓ̄ӕа̨ѧꙙа҃а̇',
+    'Д': 'ԀЂԂꚀꙢ',
+    'д': 'ԁђԃꚁꙣ',
+    'А': 'ӒӐӔѦꙘ',
+    'а': 'ӓӑӕѧꙙ',
 };
 for (const normalLetter in normalizationDictionary) {
     for (const confusingChar of splitByGlyph(normalizationDictionary[normalLetter])) {
@@ -26,9 +26,14 @@ for (const normalLetter in normalizationDictionary) {
 
 export function normalizeString(str: string): string {
     let result = '';
-    for (const char of splitByGlyph(str)) {
+    for (let char of splitByGlyph(str)) {
+        char = removeDiacriticFromCharacter(char);
         result += normalizationMap.get(char) || char;
     }
 
     return result;
+}
+
+function removeDiacriticFromCharacter(char: string): string {
+    return char.replaceAll(/(\p{Diacritic})/gu, '') || char;
 }
